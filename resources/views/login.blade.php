d837143b819df6ca02da184b902faba04a829b24
DeaNabilaHusna <112852341+DeaNabilaHusna@users.noreply.github.com>
2022-10-10 14:14:08 +0700

    Add files via upload
    menambahkan view admin, edit halaman organisasi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,52 @@
 </head>
 <body class="bg-dark">
 
-    @include('partials.navbar')
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top bg-dark">
+    <div class="container d-flex align-items-center justify-content-lg-between">
+
+      <h1 class="logo me-auto me-lg-0"><a href="/">Volunteer-an<span></span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0 sticky-top">
+        <ul>
+          <li><a class="nav-link scrollto active" href="/">Home</a></li>
+          <li><a class="nav-link scrollto" href="/#about">About</a></li>
+          <li><a class="nav-link scrollto" href="/#events">Events</a></li>
+          <li><a class="nav-link scrollto " href="/#Organizations">Organization</a></li>
+          <li><a class="nav-link scrollto" href="/#documentation">Documentation</a></li>
+          <li><a class="nav-link scrollto" href="/#contact">Contact</a></li>
+
+          {{-- <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li> --}}
+
+            {{-- </ul> --}}
+            {{-- </ul> --}}         
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#" class="get-started-btn scrollto">Login</a>
+
+    </div>
+  </header><!-- End Header -->
+
     
     <section class="container-fluid">
         <div class="container py-5 h-100">
@@ -53,7 +98,11 @@
                         </div>
       
                         <div class="pt-1 mb-4">
+<<<<<<< HEAD
                           <button class="btn btn-dark btn-lg btn-block" type="button"><a href="/volunteers">Login</a></button>
+=======
+                          <button onclick="href='/admin';" class="btn btn-dark btn-lg btn-block">Login</button>
+>>>>>>> 99c8294 (menambahkan view admin)
                         </div>
       
                         <a class="small text-muted" href="#!">Forgot password?</a>
---
+++ resources/views/partials/sidebaradmin.blade.php
@@ -0,0 +1,57 @@
+
+        <div id="sidebar">
+            <div class="sidebar-header">
+                <h3><img src="{{ asset('import/img/logo.png') }}" class="img-fluid" /><span>Volunteer-an</span></h3>
+            </div>
+            <ul class="list-unstyled component m-0">
+                <li class="active">
+                    <a href="/admin" class="dashboard"><i class="material-icons">dashboard</i>dashboard </a>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#homeSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="material-icons">diversity_3</i>Organizations
+                    </a>
+                    <ul class="collapse list-unstyled menu" id="homeSubmenu1">
+                        <li><a href="#">Fauna</a></li>
+                        <li><a href="#">Kesehatan</a></li>
+                        <li><a href="#">Lingkungan</a></li>
+                        <li><a href="#">Pendidikan</a></li>
+                        <li><a href="#">Pengembangan Masyarakat</a></li>
+                        <li><a href="#">Penanggulangan Bencana</a></li>
+                        <li><a href="#">Seni dan Budaya</a></li>
+                    </ul>
+                </li>
+
+
+                <li class="">
+                    <a href="#" class="">
+                        <i class="material-icons">supervisor_account</i>Volunteers
+                    </a>
+                </li>
+
+                <li class="">
+                    <a href="#" class="">
+                        <i class="material-icons">diversity_2</i>Events
+                    </a>
+                </li>
+
+                <li class="dropdown">
+                    <a href="#homeSubmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <i class="material-icons">equalizer</i>charts
+                    </a>
+                    <ul class="collapse list-unstyled menu" id="homeSubmenu3">
+                        <li><a href="#">Pages 1</a></li>
+                        <li><a href="#">Pages 2</a></li>
+                        <li><a href="#">Pages 3</a></li>
+                    </ul>
+                </li>
+
+                <li class="">
+                    <a href="#" class="">
+                        <i class="material-icons">settings</i>Website Settings
+                    </a>
+                </li>
+        </div>
+
+        <!-------sidebar--design- close----------->
