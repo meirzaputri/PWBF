b17e20dfe9feeb1eed91fdb6f8a63a1568cda19c
meirzaputri <meirza.putri.liandhie-2021@vokasi.unair.ac.id>
2022-11-22 11:06:34 +0700

    progress

--- resources/views/relawan/history.blade.php
+++ resources/views/relawan/history.blade.php
@@ -32,8 +32,8 @@
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">Peduli Lingkungan</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <p class="card-text"><small class="text-muted">Dilaksanakan Oleh : Go Green!</small></p>
+              <p class="card-text"><small class="" style="color: blue">Sedang Berlangsung</small></p>
             </div>
           </div>
         </div>
